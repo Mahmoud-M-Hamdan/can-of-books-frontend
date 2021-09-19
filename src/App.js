778ec167eabec6df2ed8b2ce639a16725b923e2f
Mahmoud-M-Hamdan <eng.mech.hamdan@gmail.com>
2021-09-20 00:14:14 +0300

    gds

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      data:[]
+    }
+  }
+  componentDidMount = () => {
+    
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/seed-data`).then((res) => {
+      this.setState({
+        data: res.data,
+        
+      });
+      console.log(this.state.data)
+    });
+  };
+  render() {
+    return (
+      <>
+      <h1>Data</h1>
+        {
+          this.state.data.map(post=>{
+            return <>
+            <h1>{post.title}</h1>
+            <h1>{post.description}</h1>
+            <h1>{post.status}</h1>
+            <h1>{post.email}</h1>
+            
+            </>
+          })
+        }
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
